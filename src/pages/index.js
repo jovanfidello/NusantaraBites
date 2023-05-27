4f47d3b44e263fc5ff643aa0884a8179e347f7cc
Jovan Fidello <96285805+jovanfidello@users.noreply.github.com>
2023-05-28 02:37:51 +0700

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Home from "../pages/HomePage/HomePage";
+import Error from "../pages/ErrorPage/ErrorPage";
+import MealDetails from "../pages/MealDetailsPage/MealDetailsPage";
+import Category from "../pages/CategoryPage/CategoryPage";
+import About from "../pages/AboutUs/About";
+
+
+export { Home, Error, MealDetails, Category, About};
