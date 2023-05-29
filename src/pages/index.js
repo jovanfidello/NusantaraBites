dcef0b575d0aef41e2bf5609acf8d4c3f46fbbac
Jovan Fidello <96285805+jovanfidello@users.noreply.github.com>
2023-05-29 23:57:23 +0700

    Delete src directory

--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import Home from "../pages/HomePage/HomePage";
-import Error from "../pages/ErrorPage/ErrorPage";
-import MealDetails from "../pages/MealDetailsPage/MealDetailsPage";
-import Category from "../pages/CategoryPage/CategoryPage";
-import About from "../pages/AboutUs/About";
-
-
-export { Home, Error, MealDetails, Category, About};
